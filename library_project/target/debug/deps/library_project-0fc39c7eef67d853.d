5fed117e5a5c46dfe188487a9a8d113641c7cb38
bobby3003 <roberto3003@riseup.net>
2021-09-24 06:07:10 +0700

    library

---
+++ library_project/target/debug/deps/library_project-0fc39c7eef67d853.d
@@ -0,0 +1,7 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/library_project/target/debug/deps/library_project-0fc39c7eef67d853.rmeta: src/lib.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/library_project/target/debug/deps/liblibrary_project-0fc39c7eef67d853.rlib: src/lib.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/library_project/target/debug/deps/library_project-0fc39c7eef67d853.d: src/lib.rs
+
+src/lib.rs:
