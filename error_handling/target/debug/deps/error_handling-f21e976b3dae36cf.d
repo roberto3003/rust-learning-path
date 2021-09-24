6d2783972205525fcb2945044a3d2d2b87a6efc9
bobby3003 <roberto3003@riseup.net>
2021-09-24 18:18:51 +0700

    errors

---
+++ error_handling/target/debug/deps/error_handling-f21e976b3dae36cf.d
@@ -0,0 +1,5 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/error_handling/target/debug/deps/error_handling-f21e976b3dae36cf: src/main.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/error_handling/target/debug/deps/error_handling-f21e976b3dae36cf.d: src/main.rs
+
+src/main.rs:
---
+++ error_handling/target/debug/error_handling.d
@@ -0,0 +1,1 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/error_handling/target/debug/error_handling: /home/bobby/Dev/projects/rust/rust-learning-path/error_handling/src/main.rs
