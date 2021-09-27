8033a0acafc772d3126335406a79a290f65e0e80
bobby3003 <roberto3003@riseup.net>
2021-09-27 09:38:09 +0700

    tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -39,4 +39,34 @@ mod tests {
             result
         );
     }
+
+    pub struct Guess {
+        value: u32,
+    }
+
+    impl Guess {
+        pub fn new(value: u32) -> Guess {
+            // if value < 1 || value > 100 {
+            // Add a bug:
+            if value < 1 {
+                // panic!("Guess value must be between 1 and 100, got {}.", value);
+                // Introduced a bug by swapping the bodies of if and else if:
+                panic!(
+                    "Guess value must be less than or equal to 100, got {}.",
+                    value
+                );
+            } else if value > 100 {
+                // panic!("Guess value must be less than or equal to 100, got {}.", value);
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+
+            Guess { value }
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
