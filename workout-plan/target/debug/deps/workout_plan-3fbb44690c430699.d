544a4b838ab5aafcecfb98415c49d1329ec9b4c9
bobby3003 <roberto3003@riseup.net>
2021-09-28 08:01:23 +0700

    initial commit

---
+++ workout-plan/target/debug/deps/workout_plan-3fbb44690c430699.d
@@ -0,0 +1,5 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-3fbb44690c430699.rmeta: src/main.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-3fbb44690c430699.d: src/main.rs
+
+src/main.rs:
---
+++ workout-plan/target/debug/deps/workout_plan-6ce50e9d96e96b18.d
@@ -0,0 +1,5 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-6ce50e9d96e96b18.rmeta: src/main.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-6ce50e9d96e96b18.d: src/main.rs
+
+src/main.rs:
