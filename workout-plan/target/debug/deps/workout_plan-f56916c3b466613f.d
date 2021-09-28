0607eebfea0031fd20ed52c117d0f2f26e6071d7
bobby3003 <roberto3003@riseup.net>
2021-09-28 09:10:27 +0700

    refactored

---
+++ workout-plan/target/debug/deps/workout_plan-f56916c3b466613f.d
@@ -0,0 +1,5 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-f56916c3b466613f: src/main.rs
+
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/deps/workout_plan-f56916c3b466613f.d: src/main.rs
+
+src/main.rs:
---
+++ workout-plan/target/debug/workout-plan.d
@@ -0,0 +1,1 @@
+/home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/target/debug/workout-plan: /home/bobby/Dev/projects/rust/rust-learning-path/workout-plan/src/main.rs
