77cd885ebb3af901f0e0ac38e2bfc94a647d3f7a
bobby3003 <roberto3003@riseup.net>
2021-10-31 08:05:45 +0700

    box

--- implementing_functionality/src/main.rs
+++ implementing_functionality/src/main.rs
@@ -16,6 +16,55 @@ impl Temperature {
     }
 }
 
+enum Color {
+    Brown,
+    Red,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("red"),
+            Color::Brown => println!("brown"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("depth: {:?}", self.depth);
+    }
+}
+
+struct ShippingBox {
+    color: Color,
+    dimensions: Dimensions,
+    weight: f64,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight: {:?}", self.weight);
+    }
+}
+
 fn main() {
     let hot = Temperature { degrees_f: 99.9 };
     hot.show_temp();
@@ -25,4 +74,13 @@ fn main() {
 
     let very_hot = Temperature::boiling();
     very_hot.show_temp();
+
+    let small_dimensions = Dimensions {
+        width: 1.0,
+        height: 2.0,
+        depth: 3.0,
+    };
+
+    let small_box = ShippingBox::new(5.0, Color::Red, small_dimensions);
+    small_box.print();
 }
